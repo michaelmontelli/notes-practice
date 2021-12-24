7f52f05ade9ca5a4d7babb878c978ac9c675fe8d
Michael Montelli <michael76654@yahoo.com>
2021-12-24 13:22:39 -0500

    Minor change to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -25,10 +26,6 @@ let notes = [
         important: true
     }
 ]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
